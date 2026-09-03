2c3cf6ddd9b31e48b0bc5b1201da41955ba771de
Robert Lippmann <robert.lippmann.development@gmail.com>
2026-09-03 00:32:57 -0400

    chore: bump openwebui drafter pipe version

--- python/reference_integrations/openwebui_pipe/open_webui_pipe_with_directive_drafter.py
+++ python/reference_integrations/openwebui_pipe/open_webui_pipe_with_directive_drafter.py
@@ -2,7 +2,7 @@
 title: Context Compiler Open WebUI Pipe (Directive Drafter)
 author: rlippmann
 author_url: https://github.com/rlippmann/context-compiler-example-integrations
-version: 0.10.1
+version: 0.10.2
 requirements: context-compiler>=0.9.0dev13, context-compiler-directive-drafter>=0.2.0dev5
 
 Open WebUI integration with Context Compiler directive drafter.
